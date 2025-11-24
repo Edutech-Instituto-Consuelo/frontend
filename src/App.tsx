28211d5f885810407935b934b8c18d0a38919e83
Gabriel Vinicius <112134365+gabrielxvnx@users.noreply.github.com>
2025-11-24 20:08:28 -0300

    feat: integração do footer no app e logo

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import NotFound from "./pages/NotFound";
+import Footer from "./components/Footer/footer";
 
 export default function App() {
     return (
@@ -14,6 +15,7 @@ export default function App() {
 
                 <Route path="*" element={<NotFound />} />
             </Routes>
+            <Footer />
         </div>
     );
 }
