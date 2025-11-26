53cb2422be587284092f2da0c895b5ab8ac9093e
jessica <jessicasoaresdev@gmail.com>
2025-11-26 15:01:53 -0300

    trocando caminho da logo no footer

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { tv } from "tailwind-variants";
-import logoImg from "../../assets/logo.png";
+import logoImg from "../../assets/logo_black.svg";
 
 const footer = tv({
   slots: {
