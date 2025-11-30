8d66b8614f7744c649e79dbf1cff8a51bd8ef9c5
carolgonc <carolinypeixoto@outlook.com>
2025-11-30 02:21:56 -0300

    fix: correção usando tailwind-variants e tipagem de props

--- src/card/CardHeader.tsx
+++ src/card/CardHeader.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import type { CardComponentProps } from './Card.types';
+import type { ComponentProps } from "react";
+import { tv, type VariantProps } from "tailwind-variants";
 
-const CardHeader: React.FC<CardComponentProps> = ({ children, className = '' }) => {
-    const baseClasses = 'px-[14px] py-[14px]';
-  
+const cardHeaderStyles = tv({
+  base: "px-4 py-4 space-y-1",
+});
+
+type CardHeaderSchema = VariantProps<typeof cardHeaderStyles>;
+
+interface CardHeaderProps extends ComponentProps<'div'>, CardHeaderSchema {}
+
+export function CardHeader({ className, children, ...props }: CardHeaderProps) {
   return (
-    <div className={`${baseClasses} ${className}`}>
+    <div className={cardHeaderStyles({ className })} {...props}>
       {children}
     </div>
   );
-};
-
-export default CardHeader;
+}
