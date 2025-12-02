24f75168e5db019e198ab1100ccadb3143a21e6d
carolgonc <carolinypeixoto@outlook.com>
2025-12-01 21:35:08 -0300

    fix: atualização do index com novos cards

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -3,10 +3,16 @@ import { CardHeader } from './CardHeader';
 import { CardBody } from './CardBody';
 import { CardImage } from './CardImage';
 import { CardProgress } from './CardProgress';
+import { CardTitle } from './CardTitle';
+import { CardAuthor, CardRating, CardPrice } from './CardDescription';
 
 export const Card = Object.assign(CardBase, {
   Header: CardHeader,
   Body: CardBody,
   Image: CardImage,
   Progress: CardProgress,
+  Title: CardTitle,
+  Author: CardAuthor,
+  Price: CardPrice,
+  Rating: CardRating,
 });
