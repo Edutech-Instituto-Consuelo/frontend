b5ccbda89c2be50356a75ac04397b45abef1dda3
jessica <jessicasoaresdev@gmail.com>
2025-12-02 00:38:36 -0300

    funcao para controle do acesso as rotas

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './assets/index.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Outlet, Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home.tsx'
 import Login from './pages/Login/index.tsx'
 import NotFound from './pages/NotFound.tsx'
 import Register from './pages/Register.tsx'
+import { useAuth } from './hooks/useAuth.ts'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
@@ -20,3 +21,25 @@ createRoot(document.getElementById('root')!).render(
         </BrowserRouter>
     </StrictMode>,
 )
+
+function PrivateRoute() {
+    const { user, loading } = useAuth();
+    const location = useLocation();
+    const from = location.pathname || "/";
+    const roleBasedPath = from.split("/")[1];
+    const notProtectedRoutes = ["/", "/login", "/register", "/explorar"];
+
+    if (loading)
+        return <div>Carregando...</div>;
+
+    if (user && (from === "/login" || from === "/register"))
+        return <Navigate to={"/"+user?.role} replace />;
+
+    if (user && roleBasedPath && user.role !== roleBasedPath)
+        return <Navigate to={"/"+user?.role} replace />;
+
+    if (!user && !notProtectedRoutes.includes(from))
+        return <Navigate to="/login" replace state={{ from }} />;
+
+    return <Outlet />;
+}
