b6d9b6489d83fa5fc4e8013500fa4dec260c3cf5
jessica <jessicasoaresdev@gmail.com>
2025-12-04 11:43:00 -0300

    aplicando verificacao de usuario utilizando hook

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,25 +1,29 @@
-import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
-import { useAuth } from "./hooks/useAuth";
 import logoBlack from "./assets/simbolo_black.svg";
 import { ToastContainer, Slide } from "react-toastify";
+import { useUser } from "./hooks/useUser";
 
 export function PrivateRoute() {
-    const user = useAuth();
-    const location = useLocation();
-    const from = location.pathname;
-    const roleBasedPath = from.split("/")[1];
+    const { user, loading, isAuthenticated } = useUser();
+    const role = user?.tipo_usuario;
+
+    const navigate = useNavigate();
+
+    const from = useLocation().pathname;
+    const roleBasedPath = from.split("/")[1] || "/";
+    
     const notProtectedRoutes = ["/", "/login", "/register", "/explorar"];
 
-    if (user && (from === "/login" || from === "/register"))
-        return <Navigate to={"/"+user?.tipo_usuario} replace />;
+    if (loading)
+        return <Loader />;
 
-    if (user && roleBasedPath && user?.tipo_usuario !== roleBasedPath)
-        return <Navigate to={"/"+user?.tipo_usuario} replace />;
+    if (!isAuthenticated && !notProtectedRoutes.includes(from))
+        navigate("/login", { replace: true });
 
-    if (!user && !notProtectedRoutes.includes(from))
-        return <Navigate to="/login" replace state={{ from }} />;
+    if (isAuthenticated && role !== roleBasedPath)
+        navigate("/"+role, { replace: true });
 
     return <Layout />;
 }
@@ -28,10 +32,13 @@ export function Layout() {
     return (
         <div className="grid grid-rows-[1fr_auto] h-screen">
             <Navbar />
+
             <main className="pt-navbar">
                 <Outlet />
             </main>
+
             <Footer />
+
             <ToastContainer
                 position="top-right"
                 autoClose={5000}
